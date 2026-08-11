b6c4ed16eb1af1793f5e9e4ee9e308aa0fbbdbc3
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2026-08-11 15:56:54 +0900

    feat: setup cargo workspace

--- flake.nix
+++ flake.nix
@@ -10,6 +10,15 @@
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-flake = {
+      url = "github:juspay/rust-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
   outputs =
@@ -24,34 +33,67 @@
       imports = [
         inputs.treefmt-nix.flakeModule
         inputs.git-hooks.flakeModule
+        inputs.rust-flake.flakeModules.default
+        inputs.rust-flake.flakeModules.nixpkgs
       ];
 
-      perSystem = { pkgs, config, ... }: {
-        devShells.default = pkgs.mkShellNoCC {
-          inputsFrom = [
-            config.pre-commit.devShell
-          ];
-        };
+      perSystem =
+        {
+          self',
+          pkgs,
+          config,
+          lib,
+          ...
+        }:
+        {
+          checks = lib.mapAttrs' (
+            name: crate:
+            let
+              crane-lib = config.rust-project.crane-lib;
+              args = crate.crane.args // {
+                src = config.rust-project.src;
+                pname = name;
+                cargoExtraArgs = "-p ${name}";
+                strictDeps = true;
+              };
+              cargoArtifacts = crane-lib.buildDepsOnly args;
+            in
+            lib.nameValuePair "${name}-test" (crane-lib.cargoTest (args // { inherit cargoArtifacts; }))
+          ) config.rust-project.crates;
 
-        pre-commit.settings = {
-          hooks = {
-            actionlint.enable = true;
-            deadnix.enable = true;
-            statix = {
-              enable = true;
-              settings.ignore = [
-                ".direnv/**"
-              ];
+          devShells.default = pkgs.mkShellNoCC {
+            inputsFrom = [
+              config.pre-commit.devShell
+              self'.devShells.rust
+            ];
+          };
+
+          pre-commit.settings = {
+            hooks = {
+              actionlint.enable = true;
+              deadnix.enable = true;
+              statix = {
+                enable = true;
+                settings.ignore = [
+                  ".direnv/**"
+                ];
+              };
+            };
+          };
+
+          rust-project = {
+            toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixfmt.enable = true;
+              rustfmt.enable = true;
+              rustfmt.package = config.rust-project.toolchain;
+              taplo.enable = true;
             };
           };
         };
-
-        treefmt = {
-          projectRootFile = "flake.nix";
-          programs = {
-            nixfmt.enable = true;
-          };
-        };
-      };
     };
 }
